bbf802dc352b530076325f30a677cf2e5b9556a4
ybs1164 <ybs1164@naver.com>
2025-07-29 16:47:17 +0900

    resolve test

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, resolveConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
@@ -7,6 +7,10 @@ export default defineConfig({
   base: "/",
   build: {
     rollupOptions: {
+      input: {
+        main: resolveConfig(__dirname, "index.html"),
+        404: resolveConfig(__dirname, "public/404.html"),
+      },
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom']
