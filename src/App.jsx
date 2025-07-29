72181be141cb0e37336ca92dc7a14affc6e7beed
ybs1164 <ybs1164@naver.com>
2025-07-29 16:35:48 +0900

    add base

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/games" element={<GameListPage />} />
