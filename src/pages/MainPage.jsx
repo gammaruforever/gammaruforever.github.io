08ca7d601ea502a50c77fcb26c959801046ca532
ybs1164 <ybs1164@naver.com>
2025-08-26 20:53:28 +0900

    add main page image

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -3,7 +3,7 @@ import './MainPage.css'
 function MainPage() {
   return (
     <div className="main-page">
-      <img src="https://lh3.googleusercontent.com/d/1EPzXe4LFdxraxNx8nzY12Mjlk58LuARF" alt="Gammaru" style={{width: 700, height: 500}} />
+      <img src="https://lh3.googleusercontent.com/d/1-LbYNPS-5Uz2OrUyGtpYBZ4N8yLiH6A3" alt="Gammaru" style={{width: 700, height: 500}} />
       <h1>GAMMARU 공모전 게임 아카이브</h1>
       <p>2016년부터 2025년까지의 여름/겨울 공모전 게임들을 한 곳에서!</p>
       <a href="/games">
