d36cad2088f02246604b81a9d703713032a99ca0
ybs1164 <ybs1164@naver.com>
2026-03-08 12:09:29 +0900

    modify title

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,7 +5,7 @@ function MainPage() {
     <div className="main-page">
       <img src="https://lh3.googleusercontent.com/d/1-LbYNPS-5Uz2OrUyGtpYBZ4N8yLiH6A3" alt="Gammaru" style={{width: 700, height: 500}} />
       <h1>GAMMARU 공모전 게임 아카이브</h1>
-      <p>2016년부터 2025년까지의 여름/겨울 공모전 게임들을 한 곳에서!</p>
+      <p>2016년부터 2026년까지의 여름/겨울 공모전 게임들을 한 곳에서!</p>
       <a href="/games">
         <button>공모전 게임 리스트 보기</button>
       </a>
